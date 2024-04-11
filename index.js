ac8fab0e269beaea7b294ad406de617d8c4abb3a
crazytct <156101760+crazytct@users.noreply.github.com>
2024-04-11 22:12:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,70 @@
-const {
-    Client,
-    Intents
-} = require('discord.js');
-const keepAliveServer = require('./keep_alive.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+require('dotenv').config();
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
-const bot = new Client({
-    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]
+const client1 = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
+});
+const client2 = new Client({
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
+});
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('YaY Your Bot Status Changed✨');
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
+  console.log(`🔗 Powered By RTX`);
 });
 
-bot.on('guildMemberAdd', (member) => {
-    const channelId = '1197950988057845910'; // The Channel ID you just copied
-    const welcomeMessage = `Hey <@${member.id}>! Welcome to my server!`;
-    member.guild.channels.fetch(channelId).then(channel => {
-        channel.send(welcomeMessage)
-    });
+const statusMessagesBot1 = ["Listening to Spotify"];
+const statusMessagesBot2 = ["Playing Minecraft"];
+
+let currentIndexBot1 = 0;
+let currentIndexBot2 = 0;
+
+
+async function login(client) {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log(`\x1b[36m%s\x1b[0m`, `|   🐇 Logged in as ${client.user.tag}`);
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    process.exit(1);
+  }
+}
+
+function updateStatusAndSendMessages(client, statusMessages, currentIndex) {
+  const currentStatus = statusMessages[currentIndex];
+  const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
+
+  client.user.setPresence({
+    activities: [{ name: currentStatus, type: ActivityType.Custom }],
+    status: 'dnd',
+  });
+  return (currentIndex + 1) % statusMessages.length;
+}
+
+client1.once('ready', () => {
+  console.log(`✅ Bot 1 is ready as ${client1.user.tag}`);
+  setInterval(() => {
+    currentIndexBot1 = updateStatusAndSendMessages(client1, statusMessagesBot1, currentIndexBot1);
+  }, 10000);
 });
 
-bot.on('messageCreate', (message) => {
-    if (message.content.toLowerCase() === '!ping') {
-        message.reply('Pong!');
-    }
+client2.once('ready', () => {
+  console.log(`Bot 2 is ready as ${client2.user.tag}`);
+  setInterval(() => {
+    currentIndexBot2 = updateStatusAndSendMessages(client2, statusMessagesBot2, currentIndexBot2);
+  }, 10000);
 });
 
-bot.on('messageCreate', (message) => {
-    if (message.content.toLowerCase().includes('hey bot') || message.content.toLowerCase().includes('kirixen')) {
-        message.channel.send('Hello there!');
-    }
-});
-
-bot.on('ready', () => {
-    console.log(`Bot ${bot.user.tag} is logged in!`);
-});
-
-bot.login('MTEyOTQyNDAwNDIzMTQ2NzE0OQ.GeEkEs.J84h3Gj9btuikrTGf0eZzlF4F37ViJILHbk62s').then(() => {
-    bot.user.setPresence({ activities: [{ name: 'ARCELIA', type: 'WATCHING' }], status: 'online' });
-});
+login(client1);
+login(client2);
